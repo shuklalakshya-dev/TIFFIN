a05bced9eb79876127ca336dfc9719804fcbd3e4
shuklalakshya-dev <lakshyashukla31@gmail.com>
2025-06-02 23:29:22 +0530

    ./

--- frontend/components/admin/OrderManagement.tsx
+++ frontend/components/admin/OrderManagement.tsx
@@ -6,7 +6,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { useToast } from "@/hooks/use-toast"
-
+//
 interface Order {
   _id: string
   customerInfo: {
